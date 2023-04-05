556b0a734eb17acde449a9e022cc875e3ffe6bbf
Vishal <vishal@openspaceservices.com>
2023-04-05 16:58:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get(`/api/:name/:id`, (req, res) => {
+    res.send(`Hello ${req.params.name} your id is ${req.params.id}!`)
+});
+
+app.listen(3000, () => console.log("listening on 3000..."))
